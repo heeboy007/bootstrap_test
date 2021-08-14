f1fe622087bfb6f3769aa4bff1c91548a8bdacba
heeboy007 <41155496+heeboy007@users.noreply.github.com>
2021-08-14 11:56:39 +0900

    RoR Tutorial is Done!
    So... what to do next?

--- railsapp/config/application.rb
+++ railsapp/config/application.rb
@@ -18,5 +18,6 @@ module Blog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.web_console.permissions = '172.17.0.1'
   end
 end
